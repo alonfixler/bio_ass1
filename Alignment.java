68854ead706909408b34fa723666d38b6e23c6d1
Alon Fixler <alonfixler@gmail.com>
2013-03-16 19:28:40 +0200

    'a'

---
+++ Alignment.java
@@ -0,0 +1,46 @@
+
+
+public class Alignment{
+	
+	String s1;
+	String s2;
+	int [][] scoreMatrix;
+	
+	public Alignment(String scoreMatrixFile,String s1,String s2)
+	{
+		this.s1 = s1;
+		this.s2 = s2;
+		
+	}
+	
+	public void global()
+	{
+		
+	}
+	
+	public void globalGap()
+	{
+	
+	}
+	
+	public void globalAffine()
+	{
+	
+	}
+	
+	public void local()
+	{
+		
+	}
+	
+	public void localGap()
+	{
+		
+	}
+	
+	public void localAffine()
+	{
+		
+	}
+	
+}
