897b205037962779bf93dd790f2deb4257d94b5a
Alon Fixler <alonfixler@gmail.com>
2013-03-16 19:39:36 +0200

    'a'

---
+++ LocalAlignment.java
@@ -0,0 +1,23 @@
+
+public class LocalAlignment extends Alignment{
+	
+	public LocalAlignment(String scoreMatrixFile,String s1,String s2)
+	{
+		super(scoreMatrixFile,s1,s2);
+	}
+	
+	public void local()
+	{
+		
+	}
+	
+	public void localGap()
+	{
+		
+	}
+	
+	public void localAffine()
+	{
+		
+	}
+}
